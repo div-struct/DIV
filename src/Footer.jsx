73f60e5585ee292c3121a1aa1839290ffd3ff417
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 12:25:44 +0800

    fixed the problem

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
         <footer id="hcontact">
             <div id="top">
                 <ul>
-                    <li className="git"><span class="active"><i class="fa-solid fa-headset"></i></span>&nbsp; Get in <span class="active">touch.</span></li>
+                    <li className="git"><span className="active"><i className="fa-solid fa-headset"></i></span>&nbsp; Get in <span className="active">touch.</span></li>
                     <li className="norm"><a href="">Terms & Conditions</a></li>
                     <li className="norm"><a href="">Privacy Policy</a></li>
-                    <li class="norm"><a href="">About Us</a></li>
+                    <li className="norm"><a href="">About Us</a></li>
                     <li className="norm"><a href="">Contact</a></li>
                 </ul>
             </div>
@@ -17,23 +17,23 @@ const Footer = () => {
                     <li className="norm"><a href="">About Us</a></li>
                     <li className="norm"><a href="">Contact</a></li>
                 </ul>
-                <p><span className="active"><i class="fa-solid fa-headset"></i></span>&nbsp; Get in <span class="active">touch.</span></p>
+                <p><span className="active"><i className="fa-solid fa-headset"></i></span>&nbsp; Get in <span className="active">touch.</span></p>
             </div>
             <div id="bot">
                 <ul>
-                    <li><a href="https://www.facebook.com/DIV-111873001585605/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-                    <li><a href="https://twitter.com/DivStruct?t=sx3ky_mZrOTudehqmjEgcA&s=09" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="https://www.instagram.com/divstruct/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-                    <li><a href="mailto:divstruct@gmail.com"><i class="fa-solid fa-envelope size"></i></a></li>
+                    <li><a href="https://www.facebook.com/DIV-111873001585605/" target="_blank"><i className="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="https://twitter.com/DivStruct?t=sx3ky_mZrOTudehqmjEgcA&s=09" target="_blank"><i className="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="https://www.instagram.com/divstruct/" target="_blank"><i className="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="mailto:divstruct@gmail.com"><i className="fa-solid fa-envelope size"></i></a></li>
                     <li><span className="active copyright">© Copyright 2022 Divstruct</span></li>
                 </ul>
             </div>
             <div id="hidden2">
                 <ul>
-                    <li><a href="https://www.facebook.com/DIV-111873001585605/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-                    <li><a href="https://twitter.com/DivStruct?t=sx3ky_mZrOTudehqmjEgcA&s=09" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
-                    <li><a href="https://www.instagram.com/divstruct/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
-                    <li><a href="mailto:divstruct@gmail.com"><i class="fa-solid fa-envelope size"></i></a></li>
+                    <li><a href="https://www.facebook.com/DIV-111873001585605/" target="_blank"><i className="fa-brands fa-facebook"></i></a></li>
+                    <li><a href="https://twitter.com/DivStruct?t=sx3ky_mZrOTudehqmjEgcA&s=09" target="_blank"><i className="fa-brands fa-twitter"></i></a></li>
+                    <li><a href="https://www.instagram.com/divstruct/" target="_blank"><i className="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="mailto:divstruct@gmail.com"><i className="fa-solid fa-envelope size"></i></a></li>
                 </ul>
                 <p>© Copyright 2022 Divstruct</p>
             </div>
