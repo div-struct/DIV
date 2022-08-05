48361246f2a2e22256a8d6393fd7e8574f76d682
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 15:26:13 +0800

    fixed className

--- src/Book/HomeComp/Sec3.jsx
+++ src/Book/HomeComp/Sec3.jsx
@@ -3,9 +3,9 @@ const Sec3 = () => {
         <section id="why">
             <div id="naze">
                 <div id="nande">
-                    <h2>Why <span class="active">need</span> a website?</h2>
+                    <h2>Why <span className="active">need</span> a website?</h2>
                     <br/>
-                    <h2 className="secline">Here's why having a website is <span class="active">important.</span></h2>
+                    <h2 className="secline">Here's why having a website is <span className="active">important.</span></h2>
                 </div>
                 <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291743129_778013066982263_4655250141162716902_n.png?stp=dst-png_s2048x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=x6bvbl5ogrgAX9lVNOO&tn=qfZol4kOKZsn9Ou1&_nc_ht=scontent.fceb1-3.fna&oh=03_AVJxFRXiL7TILZhrTEkPLoTtAycVJzY4lC0zZrEidgebNg&oe=6303D980"/>
             </div>
