d5d9301cacfd0b659d29857913a5a12ea1093abb
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 12:09:22 +0800

    main

---
+++ src/Book/HomeComp/Tsec1.jsx
@@ -0,0 +1,22 @@
+const Tsec1 = () => {
+    return (
+        <section id="ttemp">
+            <div id="tcompiler">
+                <div class="swiper">
+                    {/* <!-- Additional required wrapper --> */}
+                    <div class="swiper-wrapper">
+                    {/* <!-- Slides --> */}
+                    <div className="swiper-slide"><a href="https://animestocks.github.io/" target="_blank"><img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/290792641_1100961744137635_6883022765049660343_n.png?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=SBvr6HH3gB0AX8j682o&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIpOGJJmToQNC3jXQp8yIK7Di9VzchgYIeUs9g2gURwiw&oe=62FE4BDA"/></a></div>
+                    <div class="swiper-slide"><a href="/sample2/index.html" target="_blank"><img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/293567417_1692836594413454_1585337530345611632_n.png?_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=iR9R5kgSoBAAX8htD_k&_nc_ht=scontent.fceb1-2.fna&oh=03_AVIsjPmlAgklLlBYcP5BGlRpAHin2RyMI8vbGMkR2oENQw&oe=62FFEEE8"/></a></div>
+                    <div class="swiper-slide"><a href="/dist/index.html" target="_blank"><img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/292056300_5476819052381392_332164746454038928_n.png?_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_ohc=n3S1MWH1xdgAX-boN5t&_nc_ht=scontent.fceb1-2.fna&oh=03_AVLvChf_c9QMHilwaq7otua8hAZOrpDI_DfCtr6iBFlCNw&oe=630BAD40"/></a></div>
+                    <div class="swiper-slide"><a href="sample_4.html" target="_blank"><img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/290542465_740427257010671_4959706901145651273_n.png?_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=MCMsZUkNpS4AX_6sLiy&_nc_ht=scontent.fceb1-2.fna&oh=03_AVIyTJU0ScW_zgCl-2YAaehO2N1-SnxhD6XtbQILu3JzRw&oe=62FE0DCB"/></a></div>
+                    </div>
+                    {/* <!-- page animation --> */}
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Tsec1;
