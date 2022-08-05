e9eaaade2ae84953c1ee473168e5633a857c0c48
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 17:35:53 +0800

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
                     <li><a href="https://divstruct.tk/">Home</a></li>
                     <li><a href="https://samplewebsites.github.io/">Templates</a></li>
                     <li><a href="/templates">Our Works</a></li>
-                    <li><a href="https://divstruct.tk/templates">About</a></li>
+                    <li><a href="/">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
                 <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/292565482_386867043590782_5713725227048561280_n.png?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=pqd6YFoaowcAX80KMp7&_nc_ht=scontent.fceb1-3.fna&oh=03_AVK18DkctG33o3ZN5OPkVQZtYHR0Rlh-MWEjCwRIX25Wcw&oe=63050C24"/>
