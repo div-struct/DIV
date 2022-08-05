e1a52bf2e799529c8a6cc5e0f35db95664997c96
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 14:41:17 +0800

    test2

--- src/Book/Templates.jsx
+++ src/Book/Templates.jsx
@@ -1,10 +1,10 @@
-import Tsec1 from './HomeComp/Tsec1'
+
 import Tsec2 from './HomeComp/Tsec2'
 
 const Templates = () => {
     return (
         <div>
-            <Tsec1 />
+
             <Tsec2 />
         </div>
     )
