b10a914633739d9795d7c9180bdcc97fca3c112c
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-05 10:05:38 +0800

    main

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Book/Home'
+import Templates from './Book/Templates'
+
+
+const App = () => {
+    let Page
+    switch(window.location.pathname) {
+        case "/":
+            Page = Home
+            break
+        case "/templates":
+            Page = Templates
+            break
+    }
+
+    return (
+        <>
+        <Nav />
+        <Page />
+        <Footer />
+        </>
+    )
+}
+
+export default App;
