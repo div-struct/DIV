f9002f3137c4044ac1f33d420fa85899427d83b7
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-07 14:07:00 +0800

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="#/templates">Templates</a></li>
-                    <li><a href="/templates">Our Works</a></li>
+                    <li><a href="#/templates">Our Works</a></li>
                     <li><a href="/">About</a></li>
                     <li><a href="/">Contact</a></li>
                     <a href="#" id="close"><img src="#" alt="close"/></a>
