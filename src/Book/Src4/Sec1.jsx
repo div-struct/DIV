8d013da35c2e9b9f8a8d413224a98c40e0050c36
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-12 13:22:39 +0800

    Update Sec1.jsx

--- src/Book/Src4/Sec1.jsx
+++ src/Book/Src4/Sec1.jsx
@@ -1,7 +1,7 @@
 const Sec1 = () => {
     return (
         <section id="homes4">
-            <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/290453404_497219245508815_794277716937081673_n.png?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=Vsvf0K83qvMAX8SmU-J&_nc_ht=scontent.fceb1-3.fna&oh=03_AVKFysB8Kp5go9r5QJ5ZAw7YEVPNRRoSL9jkYis3ecJ9RA&oe=62F481F0"/>
+            <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/290453404_497219245508815_794277716937081673_n.png?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=XFFxURJTKy8AX9cQw0p&_nc_ht=scontent.fceb1-3.fna&oh=03_AVI2icjznc2G7qZRCEGqJuiAG8ojZEo4bxtpwtcsS0RiaA&oe=631C0EF0"/>
             <div id="promos4">
                 <h1 id="diffc">Sale 20% Off</h1>
                 <h1>On Everything</h1>
