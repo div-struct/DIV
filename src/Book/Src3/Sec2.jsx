9e800fe7adfbc44466ca0f026e2aeb41414539cd
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-15 13:28:39 +0800

    Update Sec2.jsx

--- src/Book/Src3/Sec2.jsx
+++ src/Book/Src3/Sec2.jsx
@@ -8,25 +8,25 @@ const Sec2 = () => {
                 </div>
                 <div id="swipes3">
                     <div id="products3">
-                        <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/252350892_606112797099534_87660193139115822_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=NIKpHsNizQ0AX_fgDmf&_nc_ht=scontent.fceb1-3.fna&oh=03_AVKrniqZNdCbVmlhectDkdodYNPzC2fFgSfHXrmwubAhOw&oe=62F4EF1C"/>
+                        <img src="https://drive.google.com/uc?id=1kWL0MwmIa7i4ui0n12SlD0hJGv-MB7Yf"/>
                         <h1>Product Name</h1>
                         <h2>₱300</h2>
                         <button>Order</button>
                     </div>
                     <div id="products3">
-                        <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/255564599_257560703058355_6568711312086773372_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=0c1nYU8x5pMAX-xMdko&_nc_ht=scontent.fceb1-3.fna&oh=03_AVI5mJvSi0CFoHBv7RhJLECot1-pqeOv1KItQ5ej5x3uew&oe=62F73613"/>
+                        <img src="https://drive.google.com/uc?id=1_A4Y7hyukMc6fU2YTRiFQ5X8OKwx7UGc"/>
                         <h1>Product Name</h1>
                         <h2>₱300</h2>
                         <button>Order</button>
                     </div>
                     <div id="products3">
-                        <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/280664636_367168438714416_386805714497633872_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=jUkHJiNpwSUAX9-4CVo&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIAPkrUR7TJyFFpa3x0dXDFbFduP09YQNw-UnIrf2TWgQ&oe=62F606E7"/>
+                        <img src="https://drive.google.com/uc?id=1bsFwyXJd9Ynu4Z5Glo3_WqtLD5gkvdzX"/>
                         <h1>Product Name</h1>
                         <h2>₱300</h2>
                         <button>Order</button>
                     </div>
                     <div id="products3">
-                        <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/279308982_1126955304520670_8193452801223988104_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=8bH5WxeuxdcAX-EoYS5&_nc_ht=scontent.fceb1-2.fna&oh=03_AVJcPzMm-3oZG4Z4u3Lqbo30ro1TGqEZXO73L6jdCHUQ4Q&oe=62F81ACD"/>
+                        <img src="https://drive.google.com/uc?id=1etKvlW1caVrI9RpxAFNjrbrcLycMjOF0"/>
                         <h1>Product Name</h1>
                         <h2>₱300</h2>
                         <button>Order</button>
