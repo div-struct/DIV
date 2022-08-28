cda0f5961737ba221996599b4c5671de6237cf97
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-28 11:58:44 +0800

    updated bg

--- src/Book/HomeComp/Ichi.jsx
+++ src/Book/HomeComp/Ichi.jsx
@@ -2,7 +2,7 @@ const Ichi = () => {
     return (
         <header>
             <div id="bg">
-                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/290453404_497219245508815_794277716937081673_n.png?_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGDdKrotNq60n1hi8tIVkjsONwY1_G0AoM43BjX8bQCg7xgqgB-qSYm1sH-hio9i1wMqlUWpoMURezkZWdfZCPR&_nc_ohc=cumSMxiGOzUAX_RJPTp&_nc_ht=scontent.fceb1-3.fna&oh=03_AVK0fe_6a8n2Xk-1xk8gTpJSOn47NQd5Nr0U7Yty5vaqCg&oe=632FD570" className="bg"/>
+                <img src="https://drive.google.com/uc?id=1SrZWQjegWGxBgI8NR4l46hHbMyqOyXJw" className="bg"/>
             </div>
             <div id="img1">
                 <img src="https://drive.google.com/uc?id=1HQqF_HODKg-dPZB2DXqnwDU1B_g02hAR"/>
