9ecb34aba55aed1849e8b13e2ad67f84bab95c1a
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-08-28 12:32:28 +0800

    updated

--- src/Book/HomeComp/Sec3.jsx
+++ src/Book/HomeComp/Sec3.jsx
@@ -7,7 +7,7 @@ const Sec3 = () => {
                     <br/>
                     <h2 className="secline">Here's why having a website is <span className="active">important.</span></h2>
                 </div>
-                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291743129_778013066982263_4655250141162716902_n.png?stp=dst-png_s2048x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=x6bvbl5ogrgAX9lVNOO&tn=qfZol4kOKZsn9Ou1&_nc_ht=scontent.fceb1-3.fna&oh=03_AVJxFRXiL7TILZhrTEkPLoTtAycVJzY4lC0zZrEidgebNg&oe=6303D980"/>
+                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291743129_778013066982263_4655250141162716902_n.png?stp=dst-png_s2048x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_ohc=h-gd2mZJmhAAX8IvECJ&tn=cH0VsI3ON21654jH&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIXzxnM-zXjX3iNjlFI9cXSEXy--J42bGwS1vT0yZl8ww&oe=632F5B00"/>
             </div>
         </section>
     )
