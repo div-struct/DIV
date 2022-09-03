97e27f96f5df3f51020bf14c5c36ca9cd2e2c9b4
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-09-03 17:41:12 +0800

    updated

--- src/Book/HomeComp/Tsec2.jsx
+++ src/Book/HomeComp/Tsec2.jsx
@@ -6,7 +6,7 @@ const Tsec2 = () => {
                     <div id="pic">
                         <div id="first">
                             <a id="prev" onClick={seeform}>
-                                <img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.15752-9/291986475_1022813755034273_5498905355536102299_n.png?stp=dst-png_p1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=zSZoSWVeiOkAX9Vk2tI&_nc_ht=scontent.fceb1-1.fna&oh=03_AVJKzwRTr2DIxsMp83BxJMqxrtRCRM5CW9RnjboE1NznzQ&oe=631027CC"/>
+                                <img src="https://drive.google.com/uc?id=1IaXx71819hveOwMzLG4v-islHxmHTZvl"/>
                             </a>
                         </div>
                         <div id="preview">
@@ -15,14 +15,14 @@ const Tsec2 = () => {
                                     <div id="exit">
                                         <button onClick={close}>X</button>
                                     </div>
-                                    <img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.15752-9/291986475_1022813755034273_5498905355536102299_n.png?stp=dst-png_p1080x2048&_nc_cat=101&ccb=1-7&_nc_sid=ae9488&_nc_ohc=zSZoSWVeiOkAX9Vk2tI&_nc_ht=scontent.fceb1-1.fna&oh=03_AVJKzwRTr2DIxsMp83BxJMqxrtRCRM5CW9RnjboE1NznzQ&oe=631027CC"/>
+                                    <img src="https://drive.google.com/uc?id=1IaXx71819hveOwMzLG4v-islHxmHTZvl"/>
                                     <a href="https://animestocks.tk/" target="_blank"><button>View Live Demo</button></a>
                                 </div>
                             </div>
                         </div>
                         <div id="second">
                             <a href="https://animestocks.tk/" target="_blank">
-                                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291922087_1243019783152533_6981907084709616914_n.png?stp=dst-png_s2048x2048&_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=WjaZaWr68IIAX9V1l5O&_nc_oc=AQmdAnm3eHLc9B0WvOv8FkO24ya6yCz5qzUWNt9a8FPxGS9K5tsPCvc9zOvCVnmAvwPWpG5qcuL86-QPzIfVR377&_nc_ht=scontent.fceb1-3.fna&oh=03_AVKf4R_iNQMfCKD5Vi0FaPBdumSe9rTWG5L-HH8szYay8g&oe=63110B16"/>
+                                <img src="https://drive.google.com/uc?id=17SmIRvtqOII0xeP12kLT-duoSO2ERxD7"/>
                             <div id="invi">
                                 <div id="bttn">
                                     <h2>View Live Demo</h2>
@@ -45,7 +45,7 @@ const Tsec2 = () => {
                     <div id="pic">
                         <div id="first">
                             <a id="prev" onClick={seeform2}>
-                                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291630008_367614348868917_3499367934247458943_n.png?stp=dst-png_s1080x2048&_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=55mNCr_f4tYAX8Khvuw&tn=9abK9hK7tbkWhpfa&_nc_ht=scontent.fceb1-3.fna&oh=03_AVI_gZco8fIcizf-iUz6yAyvlLaZZWI-w_d9vfZDrsq_fg&oe=63110774"/>
+                                <img src="https://drive.google.com/uc?id=1l-mUQD8dyTuPB3Jxf0lHxaYzbe83-WEO"/>
                             </a>
                         </div>
                         <div id="preview">
@@ -54,14 +54,14 @@ const Tsec2 = () => {
                                     <div id="exit">
                                         <button onClick={close2}>X</button>
                                     </div>
-                                    <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/291630008_367614348868917_3499367934247458943_n.png?stp=dst-png_s1080x2048&_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=55mNCr_f4tYAX8Khvuw&tn=9abK9hK7tbkWhpfa&_nc_ht=scontent.fceb1-3.fna&oh=03_AVI_gZco8fIcizf-iUz6yAyvlLaZZWI-w_d9vfZDrsq_fg&oe=63110774"/>
+                                    <img src="https://drive.google.com/uc?id=1l-mUQD8dyTuPB3Jxf0lHxaYzbe83-WEO"/>
                                     <a href="https://samplewebsites.github.io/" target="_blank"><button>View Live Demo</button></a>
                                 </div>
                             </div>
                         </div>
                         <div id="second">
                             <a href="https://samplewebsites.github.io/" target="_blank">
-                                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/297049964_465256278394784_6178310086300713745_n.png?stp=dst-png_s2048x2048&_nc_cat=104&ccb=1-7&_nc_sid=ae9488&_nc_ohc=oTNcdoPbf1cAX9sji0J&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIh1fUIbBeWnB1wm23OEpcO-HwdZKfTVGmQid-tTHd-tw&oe=63106DEA"/>
+                                <img src="https://drive.google.com/uc?id=1ukP9ZbrcSyKF6CV2s6YSpmsLMwJ_FtPw"/>
                             <div id="invi">
                                 <div id="bttn">
                                     <h2>View Live Demo</h2>
@@ -84,7 +84,7 @@ const Tsec2 = () => {
                     <div id="pic">
                         <div id="first">
                             <a id="prev" onClick={seeform3}>
-                                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/295323361_598414825171580_474459982482754066_n.png?stp=dst-png_s1080x2048&_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=xBgGUyegkvEAX_Ir6pY&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIqCbVtwypc_PirZ1ACXa498C_mB1oPdq9_LfvgORSmSQ&oe=63112E0C"/>
+                                <img src="https://drive.google.com/uc?id=1y0Sd0VHEBtOWmHaJlp_d6wA8OJjI7Jhv"/>
                             </a>
                         </div>
                         <div id="preview">
@@ -93,14 +93,14 @@ const Tsec2 = () => {
                                     <div id="exit">
                                         <button onClick={close3}>X</button>
                                     </div>
-                                    <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/295323361_598414825171580_474459982482754066_n.png?stp=dst-png_s1080x2048&_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=xBgGUyegkvEAX_Ir6pY&_nc_ht=scontent.fceb1-3.fna&oh=03_AVIqCbVtwypc_PirZ1ACXa498C_mB1oPdq9_LfvgORSmSQ&oe=63112E0C"/>
+                                    <img src="https://drive.google.com/uc?id=1y0Sd0VHEBtOWmHaJlp_d6wA8OJjI7Jhv"/>
                                     <a href="#/sample3" target="_blank"><button>View Live Demo</button></a>
                                 </div>
                             </div>
                         </div>
                         <div id="second">
                             <a href="#/sample3" target="_blank">
-                                <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/291392720_587486936332090_1867208350232655384_n.png?stp=dst-png_s2048x2048&_nc_cat=109&ccb=1-7&_nc_sid=ae9488&_nc_ohc=GrjOvCsG-bMAX8e-ZxD&_nc_ht=scontent.fceb1-2.fna&oh=03_AVKiuu1CAc3GIwRkVxVmATBo6yrPR7PDVNaIVSLkxDhp-g&oe=630E1BF6"/>
+                                <img src="https://drive.google.com/uc?id=1YLE8e13SAwpzVt99itKhKHUpNCeLbifC"/>
                             <div id="invi">
                                 <div id="bttn">
                                     <h2>View Live Demo</h2>
@@ -123,7 +123,7 @@ const Tsec2 = () => {
                     <div id="pic">
                         <div id="first">
                             <a id="prev" onClick={seeform4}>
-                                <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/295872208_794225561988886_3623871478003219828_n.png?stp=dst-png_s1080x2048&_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=nDzvBwYnOJIAX_cKWKE&_nc_ht=scontent.fceb1-2.fna&oh=03_AVLx1QxT5fsYzFA7hhi2iO9omxLo-dGJZ9kiAeepjmc4zw&oe=63114C74"/>
+                                <img src="https://drive.google.com/uc?id=142WAv13B2DqNyZ9nASwuX4Oa-cZ8MzIu"/>
                             </a>
                         </div>
                         <div id="preview">
@@ -132,14 +132,14 @@ const Tsec2 = () => {
                                     <div id="exit">
                                         <button onClick={close4}>X</button>
                                     </div>
-                                    <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/295872208_794225561988886_3623871478003219828_n.png?stp=dst-png_s1080x2048&_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=nDzvBwYnOJIAX_cKWKE&_nc_ht=scontent.fceb1-2.fna&oh=03_AVLx1QxT5fsYzFA7hhi2iO9omxLo-dGJZ9kiAeepjmc4zw&oe=63114C74"/>
+                                    <img src="https://drive.google.com/uc?id=142WAv13B2DqNyZ9nASwuX4Oa-cZ8MzIu"/>
                                     <a href="#/sample4" target="_blank"><button>View Live Demo</button></a>
                                 </div>
                             </div>
                         </div>
                         <div id="second">
                             <a href="#/sample4" target="_blank">
-                                <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/295585238_1232455944174672_6783190124289304113_n.png?stp=dst-png_s2048x2048&_nc_cat=110&ccb=1-7&_nc_sid=ae9488&_nc_ohc=yZ2TJSqiA2oAX_TxU9t&_nc_ht=scontent.fceb1-2.fna&oh=03_AVIYlqqW7TMaiNLyTbFm-1flwviQCydTjH09YMm8jYcg3Q&oe=63105FAD"/>
+                                <img src="https://drive.google.com/uc?id=1ZYNQoHmWhwCqWbZFRClyjWr-z0YnuL3m"/>
                             <div id="invi">
                                 <div id="bttn">
                                     <h2>View Live Demo</h2>
@@ -168,7 +168,7 @@ const Tsec2 = () => {
                         <div id="exit">
                             <button onClick={close5}>X</button>
                         </div>
-                        <img src="https://scontent.fceb1-2.fna.fbcdn.net/v/t1.15752-9/291137556_455650209424299_7315765954786413626_n.png?stp=dst-png_p1080x2048&_nc_cat=100&ccb=1-7&_nc_sid=ae9488&_nc_ohc=JOcrTGBQdhcAX8igKLG&_nc_ht=scontent.fceb1-2.fna&oh=03_AVKAyk-wiDXA2SvERnPZgjVQtgNEqEUDl0t4KzKQhZvizQ&oe=630FF7C1"/>
+                        <img src=""/>
                     </div>
                 </div>
             </div>
