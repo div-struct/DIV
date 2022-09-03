f1a1d8adac0c05b0af2cbc081390768cda012589
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-09-04 07:58:54 +0800

    updated

--- src/Book/HomeComp/Sec2.jsx
+++ src/Book/HomeComp/Sec2.jsx
@@ -3,15 +3,15 @@ const Sec2 = () => {
         <section id="addinfo">
             <div id="hcompiler">
                 <div id="hbox">
-                    <img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.15752-9/269674633_239366148347766_7045470871624881375_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeEScaDMW4mtVwEO_v9CwtVTCCed1Hxm27sIJ53UfGbbu9X990sUAaggMO5vBxmrUuzWmDfzicqoQxwEVpzrW4Fs&_nc_ohc=PLAqG_C6yHgAX-I_5m_&_nc_ht=scontent.fceb1-1.fna&oh=03_AVJCgnY3HcAwebN20R6RGpceuuytChGSS0lrS7UtGHT__g&oe=632F01A6"/>
+                    <img src="https://github.com/KellyCu/Kellycu.github.io/blob/main/images/1.jpg?raw=true"/>
                     <p>DIV team helps you create and publish your own website without knowing how to code. All you need to do is choose the available designs below or even customize your own website.</p>
                 </div>
                 <div id="hbox">
-                    <img src="https://scontent.fceb1-1.fna.fbcdn.net/v/t1.15752-9/288684985_1936689626531568_9051077596779692321_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=108&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeHuKyIqz2KdOPGxjfD36typlNjL0_9LwzSU2MvT_0vDNDsClxr_aeBrqGDXipRAVimaPNi0N1xa2cG89LN8YvXK&_nc_ohc=xzAdoWI0TygAX_asUcj&_nc_ht=scontent.fceb1-1.fna&oh=03_AVIiz2KhKkqU4jT392DPc2ZVLAUEDqw_8zykKUKQC37Wzg&oe=633209D5"/>
+                    <img src="https://github.com/KellyCu/Kellycu.github.io/blob/main/images/2.jpg?raw=true"/>
                     <p>You can send us a website template that you've made or an existing website that you wish to copy the website layout.</p>
                 </div>
                 <div id="hbox">
-                    <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/259166320_194899989398848_3530182297621136360_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=103&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeFdVbq5QgMKYrTq0LI7_m6F73wBQAndZTDvfAFACd1lMCsOvjLEusIYd0IF1EroM2idvNYgtwbYyJcWXBe7JJm_&_nc_ohc=oEZDa8muDQQAX9DRQ9d&_nc_ht=scontent.fceb1-3.fna&oh=03_AVJAAxWR5GAVIZfLLKwGzyiEcINmJW3dGl5ETntPHgkYCQ&oe=632EDCC3"/>
+                    <img src="https://github.com/KellyCu/Kellycu.github.io/blob/main/images/3.jpg?raw=true"/>
                     <p>There are plenty of unique website designs out there. You can choose which color you like for your own website.</p>
                 </div>
             </div>
