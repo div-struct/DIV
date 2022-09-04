235ce44a8fb09b5c1ed5c9105968f47c5a788cd3
いっしき <88958917+KellyCu@users.noreply.github.com>
2022-09-04 08:00:21 +0800

    updated

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-                <img src="https://scontent.fceb1-3.fna.fbcdn.net/v/t1.15752-9/292565482_386867043590782_5713725227048561280_n.png?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeHSPy78hahmjSW75lE56nGCFElo_NgltsgUSWj82CW2yEsU_4-Qwo0fKab_5bMermlFV48KE1b6GTnNtWHGVbq2&_nc_ohc=0XERDIiGuiIAX-3WxMZ&_nc_ht=scontent.fceb1-3.fna&oh=03_AVKYQduvaUut9Vl3uQXKgqjbg8t1GCoAivUYMpY3bTX6ag&oe=63308DA4"/>
+                <img src="https://github.com/KellyCu/Kellycu.github.io/blob/main/images/divlogo.png?raw=true"/>
             </nav>
         </header>
     )
