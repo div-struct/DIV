582b477ef828c8029e84971d1553df763f5df6c7
いっしき <88958917+KellyCu@users.noreply.github.com>
2023-09-21 14:37:39 +0800

    redesigned templates

---
+++ src/Book/HomeComp/Sec1.jsx
@@ -0,0 +1,20 @@
+const Sec1 = () => {
+    return (
+        <section id="about">
+            <div id="hcontainer">
+                <div id="icon">
+                    <img src="https://drive.google.com/uc?id=11ExChhtVCStR0cONqJ7Xx8NBEXIJEB0l"/>
+                </div>
+                <div id="inform">
+                    <h1>We are the &lt; /div &gt;  team.</h1>
+                    <br/>
+                    <p>It was founded on July 9th 2022 by a small team of three members: Kelly, Yel and Bermar that aims to help people create their own website without having to learn html, css or design.</p>
+                    <br/>
+                    <p>We develop websites that depends on each client's preference and decisions, and we do our best to provide the best quality website for your personal or business needs.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Sec1;
