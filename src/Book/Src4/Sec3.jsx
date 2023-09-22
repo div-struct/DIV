810fec56b66cb7fef2ae7cf035b83b38b0777ab8
いっしき <88958917+KellyCu@users.noreply.github.com>
2023-09-22 14:40:39 +0800

    New div

---
+++ src/Book/Src4/Sec3.jsx
@@ -0,0 +1,63 @@
+const Sec3 = () => {
+    return (
+        <section id="productss4">
+            <h1>Our <span id="diffc">Best Seller</span> Product</h1>
+            <div id="compiler2s4">
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+            </div>
+            <h1>Limited Edition</h1>
+            <div id="compiler2s4">
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+                <div id="items4">
+                    <img src="https://i.pinimg.com/originals/62/98/b0/6298b026a65cf80bcf9dce061e9b79c9.png"/>
+                    <h2>Item Name</h2>
+                    <p>₱100.00</p>
+                    <button type="button"><h3>Purchase</h3></button>
+                </div>
+            </div>
+            <br/>
+            <br/>
+        </section>
+    )
+}
+export default Sec3;
