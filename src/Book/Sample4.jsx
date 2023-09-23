fc328c5db4275d4158395dfebf02fa9e942e26b7
いっしき <88958917+KellyCu@users.noreply.github.com>
2023-09-23 11:02:04 +0800

    new

---
+++ src/Book/Sample4.jsx
@@ -0,0 +1,21 @@
+import Nav from './Src4/Nav'
+import Sec1 from './Src4/Sec1'
+import Sec2 from './Src4/Sec2'
+import Sec3 from './Src4/Sec3'
+import Sec4 from './Src4/Sec4'
+import Footer from './Src4/Footer'
+
+
+const Sample4 = () => {
+    return (
+        <>
+        <Nav />
+        <Sec1 />
+        <Sec2 />
+        <Sec3 />
+        <Sec4 />
+        <Footer />
+        </>
+    )
+}
+export default Sample4;
